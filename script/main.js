75e7284a8ad4b9bbd4d4410179684e92a0f66a84
mutsuki812 <mutsuki.net@gmail.com>
2026-03-14 17:21:40 +0900

    更新 Service Worker路徑

--- script/main.js
+++ script/main.js
@@ -59,7 +59,7 @@ class TaskScheduleApp {
 
     // 註冊 Service Worker
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
+      navigator.serviceWorker.register('./service-worker.js')
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
